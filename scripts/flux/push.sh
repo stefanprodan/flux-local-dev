06e7b2a02a9e03d6a89daa8eba709bf090ca0db4
Stefan Prodan <stefan.prodan@gmail.com>
2022-12-09 19:27:30 +0200

    Use flux diff
    Signed-off-by: Stefan Prodan <stefan.prodan@gmail.com>

--- scripts/flux/push.sh
+++ scripts/flux/push.sh
@@ -6,22 +6,32 @@ cluster_name='local'
 registry='localhost:5050'
 
 diff_push() {
-  artifact_name="flux-$1-sync"
+  artifact_name=$1
   artifact_path=$2
-  mkdir -p ./bin/${artifact_name}
-  flux pull artifact oci://${registry}/${artifact_name}:${cluster_name} \
-    -o ./bin/${artifact_name} &>/dev/null || mkdir -p ./bin/${artifact_name}/${artifact_path}
-  if [[ $(git diff --no-index --stat ${artifact_path} ./bin/${artifact_name}/${artifact_path} ) != '' ]]; then
-    flux push artifact oci://${registry}/${artifact_name}:${cluster_name} \
+
+  flux diff artifact oci://${artifact_name} \
+    --path="${artifact_path}" &>/dev/null || diff_exit_code=$?
+
+  if [[  ${diff_exit_code} -ne 0 ]]; then
+    flux_output=$(flux push artifact oci://${artifact_name} \
       --path="${artifact_path}" \
       --source="$(git config --get remote.origin.url)" \
-      --revision="$(git rev-parse HEAD)"
+      --revision="$(git rev-parse HEAD)" 2>&1) || exit_code=$?
+
+     oci_url=$(echo ${flux_output} | tail -n1 | awk '/to/{print $NF}')
   else
     echo "✔ no changes detected in ${artifact_path}"
+    return
   fi
-  rm -rf ./bin/${artifact_name}
+
+  if [[  ${exit_code} -ne 0 ]]; then
+    echo ${flux_output}
+    exit 1
+  fi
+
+  echo "✔ pushed to ${oci_url}"
 }
 
-diff_push cluster ${kubernetes_path}/clusters/${cluster_name}
-diff_push infra ${kubernetes_path}/infra
-diff_push apps ${kubernetes_path}/apps
+diff_push "${registry}/flux-cluster-sync:local" "${kubernetes_path}/clusters/${cluster_name}"
+diff_push "${registry}/flux-infra-sync:local" "${kubernetes_path}/infra"
+diff_push "${registry}/flux-apps-sync:local" "${kubernetes_path}/apps"
